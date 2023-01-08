cd9918641b6135b3691067fda084a910eba3277f
mohammed-yassine-dabboussi <mohammed-yassine.dabboussi@laplateforme.io>
2023-01-08 18:46:30 +0100

    suppression fichier style

--- User.php
+++ User.php
@@ -1,150 +1,89 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css"/>
-    <link rel="shortcut icon" href="" >
-    <title>Classes PHP</title>
-</head>
+<!--Partie PHP -->
+<?php
+    session_start();
 
+    class user{
+        private $id;
+        public $login="abc";
+        public $password="abc";
+        public $email="abc";
+        public $firstname="abc";
+        public $lastname="abc";
+        public $bdd;
 
-  
-<header>
-    <!--Partie gauche du header-->
-    <div class="hGauche">
-        <div class="bouton_header">Classes PHP | Mohammed Yassine Dabboussi</div>
-    </div>
-    <!--Partie droite du header-->
-    <div class="hDroite">
-        <div class="bouton_header">POO</div>
-    </div>
-</header>
+        public function __construct(){
+            $this->bdd = new mysqli('localhost', 'root', '', 'classes');
+        }
 
-    <body>
-        <!--div principale-->
-        <div class="div_body">
-        <!--div du milieu du body-->
-            <div class="div_milieu">
-                <!--formulaire 1--> 
-                <form action="" method="post">
+        //Méthode register                            
+        public function register($login, $password, $email, $firstname, $lastname){
+            $requete = "INSERT INTO utilisateurs (`login`, `password`, `email`, `firstname`, `lastname`) VALUES ('$this->login', '$this->password', '$this->email', '$this->firstname', '$this->lastname')";
+            $exec_requete = $this->bdd->query($requete);
+        }
 
-                    <table>
-                        <tr>
-                            <td>Identifiant</td>
-                            <td><input type="text" name="login"  size="25"/></td>
-                        </tr>
-                        <tr>
-                            <td>Mot de Passe</td>
-                            <td><input type="password" name="password"  size="25"/></td>
-                        </tr>
-                        <tr>
-                            <td>Email</td>
-                            <td><input type="text" name="email"  size="25"/></td>
-                        </tr>
-                        <tr>
-                            <td>Prénom</td>
-                            <td><input type="text" name="firstname"  size="25"/></td>
-                        </tr>
-                        <tr>
-                            <td>Nom</td>
-                            <td><input type="text" name="lastname"  size="25"/></td>
-                        </tr>
-                        </table>
-                        <table>
-                        <tr>
-                            <td><input type="submit" value="Ajouter" name="register"></td>
-                            <td><input type="submit" value="Supprimer" name="delete"></td>
-                        </tr>
-                        <tr>
-                            <td><input type="submit" value="Mettre à jour" name="update"></td>
-                            <td><input type="submit" value="Get All Infos" name="getallinfos"></td>
-                        </tr>
-                        <tr>
-                            <td><input type="submit" value="Get Login" name="getlogin"></td>
-                            <td><input type="submit" value="Get Email" name="getemail"></td>
-                        </tr>
-                        <tr>
-                            <td><input type="submit" value="Get Firstname" name="firstname"></td>
-                            <td><input type="submit" value="Get Lastname" name="getlastname"></td>
-                        </tr>
-                    </table>
-                </form>
-                <!--Partie PHP -->
-                <?php
-                    if (isset($_POST['login']) && isset($_POST['password']) && isset($_POST['email']) && isset($_POST['firstname']) && isset($_POST['lastname'])){
-                                
-                        class User{
-                            private $id;
-                            public $login;
-                            public $password;
-                            public $email;
-                            public $firstname;
-                            public $lastname;
-                            public $bdd;
+        //Méthode connect
+        public function connect($login_c, $password_c){
+            $requete2 = "SELECT COUNT(*) FROM `utilisateurs` WHERE `login`='abc' AND `password`='abc'";
+            $exec_requete2 = $this->bdd->query($requete2);
+            $result_fetch_array = $exec_requete2 -> fetch_array(); 
+            echo $login_c.$password_c;
+            var_dump($result_fetch_array);   
+            if($result_fetch_array['COUNT(*)'] > 1){
+                $msg="Vous êtes connecté";
+                $_SESSION['user'][]= $login_c;
+            }else {
+                $msg="Vérifier l'identifiant ou le mot de passe";
+            }
 
-                            public function __construct(){
-                                $this->bdd = new mysqli('localhost', 'root', '', 'classes');
-                                $this->login=$_POST['login'];
-                                $this->password=$_POST['password'];
-                                $this->email=$_POST['email'];
-                                $this->firstname=$_POST['firstname'];
-                                $this->lastname=$_POST['lastname'];
-                            }
-                                                        
-                            public function register($login, $password, $email, $firstname, $lastname){
-                                $requete = "INSERT INTO utilisateurs (`login`, `password`, `email`, `firstname`, `lastname`) VALUES ('$this->login', '$this->password', '$this->email', '$this->firstname', '$this->lastname')";
-                                $exec_requete = $this->bdd -> query($requete);
-                            }
-                                            
-                        }
-                    }
-                    $login=$_POST['login'];$password=$_POST['password'];$email=$_POST['email'];$firstname=$_POST['firstname'];$lastname=$_POST['lastname'];
-                    $user= new user();
-                    if ( isset($_POST['register'])){
-                        $user->register($_POST['login'], $_POST['password'], $_POST['email'], $_POST['firstname'], $_POST['lastname']);
-                        echo "Félicitations, vous êtes bien inscrit !";
-                        
-                    }
+            return $msg;
+        }
 
-                   
+        //Méthode delete 
+
+        public function delete(){
+            $requete3 = "DELETE * FROM `utilisateurs` WHERE `login`='abc' AND `password`='abc'";
+            $exec_requete3 = $this->bdd->query($requete3);
+
+            session_destroy(); 
+        }
+         
+        //Méthode delete 
+
+        public function update($login, $password, $email, $firstname, $lastname){
+            $requete3 = "UPDATE `utilisateurs` SET `id`='testupdate',`login`='testupdate',`password`='testupdate',`email`='testupdate',`firstname`='testupdate',`lastname`='testupdate' WHERE `login`='abc'";
+            $exec_requete3 = $this->bdd->query($requete3);
+        }
+
+    }
+    $login="";
+    $password="";
+    $email="";
+    $firstname="";
+    $lastname="";
+
+    $user= new user();
+    
+    //Méthode register
+        $user->register($login, $password, $email, $firstname, $lastname);
+        echo "felicitation vous êtes inscrits !";
+        echo "  <table border:1px>
+                    <tr>
+                        <td>Login</td><td>Email</td><td>First name</td><td>LAst name</td>
+                    </tr>
+                    <tr>
+                        <td>".$user->login."</td><<td>".$user->email."</td><td>".$user->firstname."</td><td>".$user->lastname."</td>
+                    </tr>
+                </table>";
+
+    //Méthode connect
+    $login_c="abc";
+    $password_c="abc";
+        echo $user->connect($login_c, $password_c);     
+        
+    //Méthode supprimer
+        $user->delete();   
+
+    //Méthode update
+        $user->update($login, $password, $email, $firstname, $lastname);
                 
-                    ?>
-            </div>
-            
-            <div class="div_milieu">
-                <!--formulaire 2--> 
-                <form action="" method="post">
-
-                    <table>
-                        <tr>
-                            <td>Identifiant</td>
-                            <td><input type="text" name="login"  size="25"/></td>
-                        </tr>
-                        <tr>
-                            <td>Mot de Passe</td>
-                            <td><input type="password" name="password"  size="25"/></td>
-                        </tr>
-                        <tr>
-                            <td><input type="submit" value="Connexion" name="connexion"></td>
-                            <td><input type="submit" value="Deconnexion" name="deconnexion"></td>
-                        </tr>
-                        <tr>
-                            <td><input type="submit" value="Is Connected" name="isconnected"></td>
-                        </tr>
-                    </table>
-                </form>
-                <?php
-                    echo "test 2";
-                ?>
-            </div>
-            </div>
-        </div>
-    </body> 
-
-    <footer>
-        <p><b>© Mohammed Yassine Dabboussi | La Plateforme | 2022-2023</b> </p>
-    </footer> 
-
-</html>
+?>
